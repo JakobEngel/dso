2e58b0856ccd932ea59e3a374a48d2b174c5dd9a
Michal Nowicki <michalsminowicki@gmail.com>
2017-06-20 23:15:01 +0200

    Correcting script

--- scripts/dsoRun.py
+++ scripts/dsoRun.py
@@ -74,7 +74,7 @@ detectionTypes = [0, 1, 1, 1, 1];
 detectionTypeFastThreshold = [-1, 20, 15, 10, 5];
 
 # Clear the results directories
-for (det, detTh) in zip(detecitonTypes, detectionTypeFastThreshold):
+for (det, detTh) in zip(detectionTypes, detectionTypeFastThreshold):
 	if not os.path.exists("results/det_"+str(det)+"_"+str(detTh)):
 		os.makedirs("results/det_"+str(det)+"_"+str(detTh));	
 	else:
