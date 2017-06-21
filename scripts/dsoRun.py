ada5864457a9b78ab13a6f890a6a34cae63009a8
Michal Nowicki <michalsminowicki@gmail.com>
2017-06-21 12:53:04 +0200

    Testing also ORBSLAM detection

--- scripts/dsoRun.py
+++ scripts/dsoRun.py
@@ -71,8 +71,8 @@ runsPerSequence = 1;
 
 mainDatasetPath = '/mnt/data/Datasets/dso';
 
-detectionTypes = [1, 1, 1];
-detectionTypeFastThreshold = [15, 10, 5];
+detectionTypes = [2, 1, 1, 1];
+detectionTypeFastThreshold = [0, 15, 10, 5];
 
 # Clear the results directories
 for (det, detTh) in zip(detectionTypes, detectionTypeFastThreshold):
