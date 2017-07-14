9cdb2c6e64e50ce60d5b8253bc425111cc76afd8
Michal Nowicki <michalsminowicki@gmail.com>
2017-07-14 13:48:38 +0200

    Let's check DSO performance without photometric calibration

--- scripts/dsoRun.py
+++ scripts/dsoRun.py
@@ -89,10 +89,10 @@ else:
 	print 'mainDatasetPath: ' + mainDatasetPath
 
 
-	detectionTypes = [6, 6, 6, 6, 7, 7, 7, 7]#[7, 7, 7, 7]; #[6, 6, 6, 6, 6, 6] #[4, 4]#[] 1, 1, 1];
-	detectionTypeFastThreshold = [0, 0, 0, 0, 0, 0, 0, 0]#[0, 0, 0, 0, 0]; #[7, 2];#[0, 15, 10, 5];
-	harrisK = [0,005, 0.01, 0.02, 0.04, 0, 0, 0, 0]#[0.01, 0.01, 0.01, 0.01];#[0.002, 0.005, 0.01, 0.02, 0.04, 0.08];
-	lambdaThreshold = [0,0,0,0, 0.000001, 0.00001, 0.0001, 0.001]#[0.001, 0.0001, 0.00001, 0.000001];
+	detectionTypes = [0]#[6, 6, 6, 6, 7, 7, 7, 7]#[7, 7, 7, 7]; #[6, 6, 6, 6, 6, 6] #[4, 4]#[] 1, 1, 1];
+	detectionTypeFastThreshold = [0]#[0, 0, 0, 0, 0, 0, 0, 0]#[0, 0, 0, 0, 0]; #[7, 2];#[0, 15, 10, 5];
+	harrisK = [0]#[0,005, 0.01, 0.02, 0.04, 0, 0, 0, 0]#[0.01, 0.01, 0.01, 0.01];#[0.002, 0.005, 0.01, 0.02, 0.04, 0.08];
+	lambdaThreshold = [0]#[0,0,0,0, 0.000001, 0.00001, 0.0001, 0.001]#[0.001, 0.0001, 0.00001, 0.000001];
 
 	# Clear the results directories
 	for (det, lamb) in zip(detectionTypes, lambdaThreshold):
@@ -101,6 +101,8 @@ else:
 		else:
 			call('rm results/det_'+str(det)+'_'+str(lamb) +'/*', shell=True);
 
+	#mode = 0; # With photometric calibration
+	#mode = 1; # Without photometric calibration
 	for seq in sequences:
 
 		for (det, fastThr, hark, lamb) in zip(detectionTypes, detectionTypeFastThreshold, harrisK, lambdaThreshold):
@@ -108,10 +110,10 @@ else:
 			for runId in range(0, runsPerSequence):
 				print("Current sequence: " + seq);
 
-				print('./dso_dataset files='+mainDatasetPath+'/sequence_'+ seq +'/images.zip calib='+mainDatasetPath+'/sequence_'+ seq +'/camera.txt gamma='+mainDatasetPath+'/sequence_'+ seq +'/pcalib.txt vignette='+mainDatasetPath+'/sequence_'+ seq +'/vignette.png preset=0 mode=0 nogui=1 reverse=0 quiet=1 detectionType=' + str(det) + ' detectionTypeFastThreshold=' +str(fastThr) +' harrisK=' + str(hark) +' lambdaThreshold=' + str(lamb));
+				print('./dso_dataset files='+mainDatasetPath+'/sequence_'+ seq +'/images.zip calib='+mainDatasetPath+'/sequence_'+ seq +'/camera.txt gamma='+mainDatasetPath+'/sequence_'+ seq +'/pcalib.txt vignette='+mainDatasetPath+'/sequence_'+ seq +'/vignette.png preset=0 mode=' + str(mode) +' nogui=1 reverse=0 quiet=1 detectionType=' + str(det) + ' detectionTypeFastThreshold=' +str(fastThr) +' harrisK=' + str(hark) +' lambdaThreshold=' + str(lamb));
 
 				# Copy to currently used settings
-				call('./dso_dataset files='+mainDatasetPath+'/sequence_'+ seq +'/images.zip calib='+mainDatasetPath+'/sequence_'+ seq +'/camera.txt gamma='+mainDatasetPath+'/sequence_'+ seq +'/pcalib.txt vignette='+mainDatasetPath+'/sequence_'+ seq +'/vignette.png preset=0 mode=0 nogui=1 reverse=0 quiet=1 detectionType=' + str(det) + ' detectionTypeFastThreshold=' +str(fastThr) +' harrisK=' + str(hark) +' lambdaThreshold=' + str(lamb), shell=True);
+				call('./dso_dataset files='+mainDatasetPath+'/sequence_'+ seq +'/images.zip calib='+mainDatasetPath+'/sequence_'+ seq +'/camera.txt gamma='+mainDatasetPath+'/sequence_'+ seq +'/pcalib.txt vignette='+mainDatasetPath+'/sequence_'+ seq +'/vignette.png preset=0 mode=' + str(mode) +' nogui=1 reverse=0 quiet=1 detectionType=' + str(det) + ' detectionTypeFastThreshold=' +str(fastThr) +' harrisK=' + str(hark) +' lambdaThreshold=' + str(lamb), shell=True);
 
 				# Run software
 				call('mv result.txt results/det_'+str(det)+'_'+str(lamb) +'/sequence_' + str(seq) + '_' + str(runId) + '.txt', shell=True);
