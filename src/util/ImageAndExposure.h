b1893bd81c9bb0216c221687d4df5c4cc68730ab
Patrick Geneva <pgeneva@udel.edu>
2017-11-28 17:41:50 -0500

    Comments cleanup from merge

--- src/util/ImageAndExposure.h
+++ src/util/ImageAndExposure.h
@@ -32,46 +32,57 @@ namespace dso
 
 
 /**
-* This class is used to transport the image data, exposure time, w, h, and timestamp throughout the
-* system. TODO it may me useful to give this class the stereo pair instead of just the mono image 
-* already here. 
-*
-* See https://docs.opencv.org/3.1.0/d3/d63/classcv_1_1Mat.html -- cv::Mat(int rows, int cols, int type, void *data, size_t step=AUTO_STEP). 
-* This is a constructor for a cv::Mat that can be used with a float* so we can use opencv's depth map functions
-*
-* - Nate
-*/
-
+ * This class is used to transport the image data, exposure time, w, h,
+ * and timestamp throughout the system.
+ * TODO: it may me useful to give this class the stereo pair instead of just the mono imagealready here.
+ * See https://docs.opencv.org/3.1.0/d3/d63/classcv_1_1Mat.html -- cv::Mat(int rows, int cols, int type, void *data, size_t step=AUTO_STEP).
+ * This is a constructor for a cv::Mat that can be used with a float* so we can use opencv's depth map functions
+ */
 class ImageAndExposure
 {
 public:
+
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 	float* imageL;			// irradiance. between 0 and 256 (stereo rectified)
 	float* imageR;			// irradiance. between 0 and 256 (stereo rectified)
 	int w,h;				// width and height;
-	double timestamp;
+	double timestamp;       // timestamp of this image if we have it
 	float exposure_time;	// exposure time in ms.
 	Vec3 p_2in1;			// second camera seen from the first
     Mat33 R_1to2;			// rotation from first to second camera
+
+    /**
+     * Default constructor
+     * Will make a set of empty images to size
+     */
     inline ImageAndExposure(int w_, int h_, double timestamp_=0) : w(w_), h(h_), timestamp(timestamp_)
 	{
 		imageL = new float[w*h];
 		imageR = new float[w*h];
 		exposure_time=1;
 	}
+
+    /**
+     * Deconstructor
+     * deletes our allocated image arrays
+     */
 	inline ~ImageAndExposure()
 	{
 		delete[] imageL;
 		delete[] imageR;
 	}
 
-	// Simply copies this.exposure_time to other.exposure_time
+	/**
+	 * Simply copies this.exposure_time to other.exposure_time
+	 */
 	inline void copyMetaTo(ImageAndExposure &other)
 	{
 		other.exposure_time = exposure_time;
 	}
 
-	// Get a copy of the object that points to a separate place in memory.
+	/**
+	 * Get a copy of the object that points to a separate place in memory.
+	 */
 	inline ImageAndExposure* getDeepCopy()
 	{
 		ImageAndExposure* img = new ImageAndExposure(w,h,timestamp);
